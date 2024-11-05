798df273489439bad4a76b77cb8269d316a41ff4
Alexander Lyashuk <crem@google.com>
2024-11-05 02:47:02 -0800

    [XLA:GPU] Require packed dot operands to be packed along contracting dimension.
    PiperOrigin-RevId: 693273185

--- third_party/xla/xla/service/gpu/transforms/layout_assignment.h
+++ third_party/xla/xla/service/gpu/transforms/layout_assignment.h
@@ -63,6 +63,12 @@ class GpuLayoutAssignment : public LayoutAssignment {
                                    absl::Span<const int64_t> row_dims,
                                    absl::Span<const int64_t> col_dims);
 
+  absl::Status SetDotOperandLayoutToMinorContracting(
+      const HloInstruction* instruction, int64_t operand,
+      absl::Span<const int64_t> batch_dims,
+      absl::Span<const int64_t> contracting_dims,
+      absl::Span<const int64_t> noncontracting_dims);
+
   absl::Status SetDotLayout(const HloInstruction* instruction,
                             LayoutConstraints* constraints);
 
